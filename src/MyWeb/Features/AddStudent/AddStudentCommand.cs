b15013ad214245aa5777a95aa71cb00b492aa3bd
wk <somnuk.wk@gmail.com>
2023-11-12 06:50:04 +0700

    Refactor adding student feature
    Deleted the old AddStudent file and replaced it with a new one, AddStudentCommand.cs. The refactored feature now takes advantage of the MediatR library for command handling. This change makes it more concise and structurally organized, improving the overall code maintainability and readability.

---
+++ src/MyWeb/Features/AddStudent/AddStudentCommand.cs
@@ -0,0 +1,24 @@
+using MediatR;
+using MyWeb.Services;
+
+namespace MyWeb.Features.AddStudent;
+
+public record AddStudentCommand(string Email, string FirstName, string LastName) : IRequest<Result<bool>>;
+
+public class AddStudentHandler : IRequestHandler<AddStudentCommand, Result<bool>> {
+    private readonly DbService _dbService;
+
+    public AddStudentHandler(DbService dbService) {
+        _dbService = dbService;
+    }
+
+    public async Task<Result<bool>> Handle(AddStudentCommand request, CancellationToken cancellationToken) {
+        var student = new Student {
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LatName = request.LastName
+        };
+        var result = _dbService.AddStudent(student);
+        return await Task.FromResult(result);
+    }
+}
